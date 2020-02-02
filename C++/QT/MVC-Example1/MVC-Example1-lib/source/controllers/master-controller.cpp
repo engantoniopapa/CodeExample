3b5f592cab96ddbc0c05c7b0244822b7e9662ac3
Antonio Papa <eng.antonio.papa@gmail.com>
2020-02-02 20:35:41 +0000

    QT Example: 2 - MVC-Example1

---
+++ C++/QT/MVC-Example1/MVC-Example1-lib/source/controllers/master-controller.cpp
@@ -0,0 +1,15 @@
+#include "master-controller.h"
+
+
+
+
+namespace MVC_Example1 {
+namespace controllers {
+
+MasterController::MasterController(QObject* parent)
+: QObject(parent)
+{
+     //message = "Ciao";
+}
+
+}}
